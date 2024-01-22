3cc51a61a9b0a69b47249e993e0b7edfa399a67d
tomerv5 <tomer.vaener@post.runi.ac.il>
2024-01-22 20:48:28 +0200

    Divisors Add

--- Divisors.java
+++ Divisors.java
@@ -3,6 +3,14 @@
  */
 public class Divisors {
 	public static void main (String[] args) {
-		//// Put your code here
+		int num = Integer.parseInt(args[0]);
+		int i = 1;
+		while (i<=num) {
+			if (num % i == 0) {
+				System.out.println(i);
+			}
+			i++;
+		}
 	}
 }
+
