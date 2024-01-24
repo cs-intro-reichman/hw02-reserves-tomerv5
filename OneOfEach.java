328a678cd691442398372bef00de3217e8448e30
tomerv5 <tomer.vaener@post.runi.ac.il>
2024-01-24 13:05:15 +0200

    OneOfEach.java Added

--- OneOfEach.java
+++ OneOfEach.java
@@ -5,6 +5,18 @@
  */
 public class OneOfEach {
 	public static void main (String[] args) {
-		//// Put your code here
+		double randomGender = Math.random();
+		char gender;
+		char previousChild = '0';
+		int numOfchildren = 0;
+		while ((gender = (randomGender < 0.5 ? 'g' : 'b')) == previousChild || numOfchildren == 0) { //Assigning new value to gender and checking equlity to previousChild.
+			System.out.print(gender + " ");
+			previousChild = gender;
+			numOfchildren++;
+			randomGender = Math.random();
+		}
+		numOfchildren++;
+		System.out.print(gender + "\nYou made it... and you now have " + numOfchildren + " children.");
+		
 	}
 }
